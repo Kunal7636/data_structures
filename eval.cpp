72ac32760aae1377518b5458776084f40d236f92
Kunal7636 <98kunalkapoor@gmail.com>
2019-10-22 12:02:18 +0530

    evaluation of prefix

---
+++ eval.cpp
@@ -0,0 +1,62 @@
+//input given as string +9*26
+//use -'0' to convert string to number
+//output 21 we scanned from last to start
+#include<iostream>
+#include<stack>
+#include<cstring>
+#include<string>
+#include<stdlib.h>
+using namespace std;
+class mystack
+{
+    public:
+	    stack<int> s;
+
+};
+int main(int argc,char* argv[])
+{
+
+	mystack o1;
+	char* str=argv[1];
+	int l=strlen(str);
+	for(int i=l-1;i>=0;i--)
+	{
+		char ch=str[i];
+		if(ch>='0'&&ch<='9')
+		{
+			
+			o1.s.push(ch-'0');
+		}
+		else
+		{
+			int a=o1.s.top();
+			o1.s.pop();
+			int b=o1.s.top();
+			o1.s.pop();
+		
+			switch(ch)
+			{
+                               case '+':
+				       o1.s.push(a+b);
+				
+				       break;
+		               case '-':
+				       o1.s.push(a-b);
+
+				       break;
+			       case '*':
+				       o1.s.push(a*b);
+				        
+                                       break;
+			       case '/':
+				       o1.s.push(a/b);
+				        
+                                       break;
+			}
+			
+		}
+	}
+	cout<<o1.s.top();
+	return 0;
+
+}
